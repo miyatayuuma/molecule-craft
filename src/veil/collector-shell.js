2e5e459d69cd0b5ca4f7ffddccbf8f536e57410d
miyatayuuma <115095547+miyatayuuma@users.noreply.github.com>
2026-09-07 17:13:57 +0900

    Restore explorer rocket silhouette

--- src/veil/collector-shell.js
+++ src/veil/collector-shell.js
@@ -6,12 +6,9 @@ export const TANK_PRESENTATION=Object.freeze({
 });
 
 export function drawCollectorShell(ctx,{x=0,y=0,angle=0,scale=1,bank=0}={}){
-  ctx.save();ctx.translate(x,y);ctx.rotate(angle);ctx.scale(scale*1.2,scale*1.2*(1-Math.abs(bank)*.12));
-  ctx.strokeStyle='#77b7c9';ctx.globalAlpha=.24;ctx.lineWidth=1;ctx.beginPath();ctx.moveTo(-15,0);ctx.bezierCurveTo(-30,-5,-42,7,-58,0);ctx.stroke();
-  ctx.globalAlpha=.42;ctx.beginPath();ctx.ellipse(0,0,22,17,0,0,Math.PI*2);ctx.stroke();
-  ctx.globalAlpha=1;ctx.fillStyle='#eaf9fc';ctx.strokeStyle='#b2eaff';ctx.lineWidth=1.6;ctx.beginPath();ctx.ellipse(0,0,14,11,0,0,Math.PI*2);ctx.fill();ctx.stroke();
-  ctx.fillStyle='#153e58';ctx.beginPath();ctx.ellipse(4,0,6,7,0,0,Math.PI*2);ctx.fill();ctx.strokeStyle='#88d6e5';ctx.beginPath();ctx.arc(4,0,9,-.72,.72);ctx.stroke();
-  ctx.fillStyle='#bff4fa';ctx.beginPath();ctx.arc(-5,0,2.2,0,Math.PI*2);ctx.fill();ctx.restore();ctx.globalAlpha=1;
+  ctx.save();ctx.translate(x,y);ctx.rotate(angle);ctx.scale(scale*1.2,scale*1.2*(1-Math.abs(bank)*.18));
+  ctx.fillStyle='#f1fbff';ctx.strokeStyle='#b2eaff';ctx.lineWidth=2;ctx.beginPath();ctx.moveTo(17,0);ctx.quadraticCurveTo(-7,-14,-12,-8);ctx.lineTo(-6,0);ctx.lineTo(-12,8);ctx.quadraticCurveTo(-7,14,17,0);ctx.fill();ctx.stroke();
+  ctx.fillStyle='#153e58';ctx.beginPath();ctx.arc(0,0,3,0,Math.PI*2);ctx.fill();ctx.restore();ctx.globalAlpha=1;
 }
 
 export function drawCollectorShellPreview(canvas){
