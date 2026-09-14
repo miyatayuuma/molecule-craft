7cf8627e6895cfa90dac47d03c698baa0db5373f
miyatayuuma <115095547+miyatayuuma@users.noreply.github.com>
2026-09-14 14:57:50 +0900

    Add shared Nitrogen FIELD geometry config

---
+++ src/veil/nitrogen-config.js
@@ -0,0 +1,5 @@
+export const NITROGEN_REGION_ID='nitrogen';
+export const NITROGEN_REGION_Y=-12850;
+export const NITROGEN_REGION_BOUNDS=Object.freeze({left:-1100,right:1250,top:-16650,bottom:500});
+export const NITROGEN_ENTRY=Object.freeze({x:280,y:-12920,angle:-Math.PI/2});
+export const NITROGEN_EXIT=Object.freeze({x:120,y:-16080,angle:-Math.PI/2});
