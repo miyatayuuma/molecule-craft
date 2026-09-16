13be5ec988b9f789e341722c66a2d891f6c146de
miyatayuuma <115095547+miyatayuuma@users.noreply.github.com>
2026-09-16 10:38:13 +0900

    Install Rare Survey resource lifecycle

--- src/craft-connections.js
+++ src/craft-connections.js
@@ -7,6 +7,7 @@ import {loadMoleculeGraph} from './molecule-graph.js';
 import {CRITICAL_INSIGHT_IDS} from './veil/insights.js';
 import {primaryRoleFor} from './veil/molecule-roles.js';
 import {installTankCapabilityPresentation} from './veil/capability-unlock.js?v=1';
+import {installRareSurvey} from './veil/rare-survey.js';
 import {presentFirstRegistration,REGISTRATION_REVEAL_HOLD_MS} from './collection-registration-reveal.js?v=1';
 
 function normalizeExplorationMode(){
@@ -63,7 +64,7 @@ export async function prepareExplorationCatalog(resources){
 }
 
 function createReadyExploration({resources,canLeave,canSupply,onBeforeLaunch,onCraft,onCommit,reset}){
-  installEmptyDeparturePolicy(resources);
+  installRareSurvey(resources);installEmptyDeparturePolicy(resources);
   const pendingCraft=installPendingCraftAccess({resources});
   const veilUI=createVeilUI({resources,canLeave,canSupply,onBeforeLaunch,onCraft:(...args)=>{pendingCraft.refresh();return onCraft(...args);},onCommit});
   const capabilityPresentation=installTankCapabilityPresentation({resources}),forwardDiscovery=veilUI.discovered?.bind(veilUI);
